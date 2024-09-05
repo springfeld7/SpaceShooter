b12eda8c3a08db95ac2eece060a236dc810831a9
thsp1900 <thsp1900@student.miun.se>
2024-09-05 20:15:38 +0200

    Deleted  unused import directives

--- app/src/main/java/com/thomasspringfeldt/spaceshooter/MainActivity.kt
+++ app/src/main/java/com/thomasspringfeldt/spaceshooter/MainActivity.kt
@@ -3,8 +3,6 @@ package com.thomasspringfeldt.spaceshooter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
