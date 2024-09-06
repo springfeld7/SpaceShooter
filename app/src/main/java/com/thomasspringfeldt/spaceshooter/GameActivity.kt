92800a0b5960055fd930a7eb4465ce7d6515f85d
thsp1900 <thsp1900@student.miun.se>
2024-09-06 10:26:25 +0200

    Ovverrode some lifecycle functions

--- app/src/main/java/com/thomasspringfeldt/spaceshooter/GameActivity.kt
+++ app/src/main/java/com/thomasspringfeldt/spaceshooter/GameActivity.kt
@@ -4,10 +4,29 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * Activity for the game screen.
+ */
 class GameActivity : AppCompatActivity() {
     private final val TAG = "GameActivity"
+    lateinit var game : Game
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        game = Game(this)
+        setContentView(game)
         Log.d(TAG, "Game activity was launched")
     }
+
+    override fun onPause() {
+        game.pause()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        game.resume()
+    }
+
+
 }
