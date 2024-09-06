a3dfc39dccc9c74a5e9ba556cb8c430602aed64c
thsp1900 <thsp1900@student.miun.se>
2024-09-06 10:25:55 +0200

    Started building the Game class

--- app/src/main/java/com/thomasspringfeldt/spaceshooter/Game.kt
+++ app/src/main/java/com/thomasspringfeldt/spaceshooter/Game.kt
@@ -1,4 +1,35 @@
 package com.thomasspringfeldt.spaceshooter
 
-class Game {
+import android.content.Context
+import android.view.SurfaceView
+
+/**
+ * Game engine for the Space Shooter.
+ * @author Thomas Springfeldt
+ */
+class Game(context: Context?) : SurfaceView(context), Runnable {
+
+    /**
+     * Game loop.
+     */
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Stops the game from processing.
+     */
+    fun pause() {
+
+    }
+
+    /**
+     * Resumes the processing of the game.
+     */
+    fun resume() {
+        //start the new thread
+        //set isRunning = true
+        //start the thread
+    }
+
 }
