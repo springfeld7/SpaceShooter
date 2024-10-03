2ecaba01882f1c93676709c60be021b54bef29c4
thsp1900 <thsp1900@student.miun.se>
2024-10-03 08:33:22 +0200

    Deleted log outputs

--- app/src/main/java/com/thomasspringfeldt/spaceshooter/GameActivity.kt
+++ app/src/main/java/com/thomasspringfeldt/spaceshooter/GameActivity.kt
@@ -1,19 +1,17 @@
 package com.thomasspringfeldt.spaceshooter
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 /**
  * Activity for the game screen.
  */
 class GameActivity : AppCompatActivity() {
-    private val TAG = "GameActivity"
-    lateinit var game : Game
+    private val tag = "GameActivity"
+    private lateinit var game : Game
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "Game activity was launched")
         game = Game(this)
         setContentView(game)
 
--- app/src/main/java/com/thomasspringfeldt/spaceshooter/MainActivity.kt
+++ app/src/main/java/com/thomasspringfeldt/spaceshooter/MainActivity.kt
@@ -2,7 +2,6 @@ package com.thomasspringfeldt.spaceshooter
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -12,13 +11,12 @@ import androidx.appcompat.app.AppCompatActivity
  * Activity for the main starting screen.
  */
 class MainActivity : AppCompatActivity() {
-    private val TAG = "MainActivity"
+    private val tag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.start_button)?.setOnClickListener {
-            Log.d(TAG, "Start button was pressed")
             val intent = Intent(this, GameActivity::class.java)
             startActivity(intent)
         }
