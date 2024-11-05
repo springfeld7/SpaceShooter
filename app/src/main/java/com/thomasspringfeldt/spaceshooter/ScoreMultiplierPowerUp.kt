a7e6ab102f9f4d590e7b7a8b85ac0e7b039d4475
thsp1900 <thsp1900@student.miun.se>
2024-11-05 14:01:39 +0100

    Added two powerups

---
+++ app/src/main/java/com/thomasspringfeldt/spaceshooter/ScoreMultiplierPowerUp.kt
@@ -0,0 +1,46 @@
+package com.thomasspringfeldt.spaceshooter
+
+const val SCORE_MULTIPLIER_PWRUP_DURATION = 10000
+
+/**
+ * Score multiplier power up.
+ * @author Thomas Springfeldt
+ */
+class ScoreMultiplierPowerUp(game: Game, player: Player) : PowerUp() {
+
+    init {
+        velX = PWRUP_VELOCITY
+        pwrUpDuration = INVINC_PWRUP_DURATION
+        val id = R.drawable.pwrup_invinc
+        bitmap = createScaledBitmap(game, id)
+        width = bitmap.width.toFloat()
+        height = bitmap.height.toFloat()
+        super.player = player
+        left = STAGE_WIDTH.toFloat() + RNG.nextInt(STAGE_WIDTH)
+        centerY = RNG.nextInt((height / 2).toInt(), STAGE_HEIGHT - (height / 2).toInt()).toFloat()
+    }
+
+    override fun update(playerVelocity: Float) {
+        super.update()
+
+        if (!isActive) {
+            x -= playerVelocity + velX
+        }
+
+        if (isActive && System.currentTimeMillis() - timer >= pwrUpDuration) {
+            player.isInvincible = false
+            isDead = true
+        }
+        if (isActive && !isDead) { player.handleIFrames(INVINC_PWRUP_DURATION) }
+
+    }
+
+    override fun onCollision(that: Entity) {
+        super.onCollision(that)
+
+        left = -width
+        timer = System.currentTimeMillis()
+        isActive = true
+
+    }
+}
