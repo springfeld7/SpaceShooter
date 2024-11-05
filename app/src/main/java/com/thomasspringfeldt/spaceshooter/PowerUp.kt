3d85492f64549730a915f4231bd4711e55cbe15d
thsp1900 <thsp1900@student.miun.se>
2024-11-05 14:02:11 +0100

    Refactored powerup hierarchy

--- app/src/main/java/com/thomasspringfeldt/spaceshooter/PowerUp.kt
+++ app/src/main/java/com/thomasspringfeldt/spaceshooter/PowerUp.kt
@@ -2,6 +2,8 @@ package com.thomasspringfeldt.spaceshooter
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
 
 const val PWRUP_VELOCITY = 8f
 
@@ -11,10 +13,19 @@ const val PWRUP_VELOCITY = 8f
  */
 abstract class PowerUp : Entity() {
 
-    private var duration = 0
-    private var empty = false
+    lateinit var bitmap: Bitmap
+    var pwrUpDuration = 0
+    var isActive = false
+    var isDead = false
+    var timer :Long = 0
+    lateinit var player : Player
 
     abstract fun update(playerVelocity: Float)
+
+    override fun render(canvas: Canvas, paint: Paint) {
+        super.render(canvas, paint)
+        canvas.drawBitmap(bitmap, x, y, paint)
+    }
 
     fun createScaledBitmap(game: Game, resId: Int) : Bitmap {
         val original = BitmapFactory.decodeResource(game.resources, resId)
@@ -24,10 +35,4 @@ abstract class PowerUp : Entity() {
         val scaled = Bitmap.createScaledBitmap(original, newW, newH, true)
         return scaled
     }
-
-    fun setDuration(duration: Int) { this.duration = duration}
-    fun getDuration() : Int { return duration }
-
-    fun isEmpty() : Boolean { return empty }
-    fun flipEmpty() { empty = !empty }
 }
