c89852932b2725940cbe4b91081857144bb9fa54
thsp1900 <thsp1900@student.miun.se>
2024-11-05 15:02:02 +0100

    Deleted whitespace

--- app/src/main/java/com/thomasspringfeldt/spaceshooter/InvincibilityPowerUp.kt
+++ app/src/main/java/com/thomasspringfeldt/spaceshooter/InvincibilityPowerUp.kt
@@ -8,8 +8,6 @@ const val INVINC_PWRUP_DURATION = 8000
  */
 class InvincibilityPowerUp(game: Game, player: Player) : PowerUp() {
 
-
-
     init {
         velX = PWRUP_VELOCITY
         pwrUpDuration = INVINC_PWRUP_DURATION
@@ -18,7 +16,7 @@ class InvincibilityPowerUp(game: Game, player: Player) : PowerUp() {
         width = bitmap.width.toFloat()
         height = bitmap.height.toFloat()
         super.player = player
-        left = STAGE_WIDTH.toFloat() + RNG.nextInt(STAGE_WIDTH)
+        left = STAGE_WIDTH.toFloat() + RNG.nextInt(STAGE_WIDTH * 2)
         centerY = RNG.nextInt((height / 2).toInt(), STAGE_HEIGHT - (height / 2).toInt()).toFloat()
     }
 
@@ -28,13 +26,11 @@ class InvincibilityPowerUp(game: Game, player: Player) : PowerUp() {
         if (!isActive) {
             x -= playerVelocity + velX
         }
-
         if (isActive && System.currentTimeMillis() - timer >= pwrUpDuration) {
             player.isInvincible = false
             isDead = true
         }
         if (isActive && !isDead) { player.handleIFrames(INVINC_PWRUP_DURATION) }
-
     }
 
     override fun onCollision(that: Entity) {
